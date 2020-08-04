217a9dcc41dda8618fe0764fd84069882513b41e
BlueGeneWW <2322447032@qq.com>
2020-08-04 16:17:31 +0800

    编写工厂类和配置文件，工厂模式解耦。

--- day01_eesy_02factory/src/main/java/com/itheima/ui/Client.java
+++ day01_eesy_02factory/src/main/java/com/itheima/ui/Client.java
@@ -11,13 +11,13 @@ public class Client {
      * 1.单例对象是说从始至终只有一个对象实例，对象只被创建了一次，
      *   从而类中的成员也就只会初始化一次：servlet就是一个单例对象
      *
-     * 2.多例对象，对象被创建多次，执行效率没有多例对象高
+     * 2.多例对象，对象被创建多次，执行效率没有单例对象高
      */
     public static void main(String[] args) {
         //IAccountService as = new AccountServiceImpl();
         for(int i=0;i<5;i++) {
             IAccountService as = (IAccountService) BeanFactory.getBean("accountService");
-            System.out.println(as);//多例对象
+            System.out.println(as);//dan例对象
             as.saveAccount();
         }
 
